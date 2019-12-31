e11db5ef4417cc30cd80e553876ddd1abb613550
Gurjap Singh <gurjapsingh@Gurjaps-MacBook-Air-2.local>
2019-12-31 11:17:05 -0500

    before switching to strings

--- content.js
+++ content.js
@@ -1,16 +1,38 @@
-function changeOrder(list){
+var balance_list_asc = []
+var balance_list_desc = []
+var testList_1 = []
+var testList_2 = []
+var transaction_list = []
+function changeOrder(list,tl){
     transaction_table = document.querySelector("#ccDetailsTransactionAccountAccess > tbody")
-    console.log(transaction_table.children)
     for(var i = 1,j=0; i < transaction_table.children.length; i++,j++){
         var row = transaction_table.children[i]
-        console.log(row)
-        console.log(list[j])
-        row.appendChild(list[j].debit)
+        console.log(transaction_list[j])
+        var new_col = document.createElement("td")
+        new_col.innerText = "pay"
+        console.log(balance_list_asc[j].debit)
+        row.appendChild(balance_list_asc[j].debit)
         row.appendChild(list[j].credit)
         row.appendChild(list[j].balance)
     }
 }
-
+function myFunction() {
+    console.log("welcome to my function")
+    setTimeout( () => {
+        console.log(transaction_date)
+        if (transaction_date.classList.contains("sortedDesc") == true){
+            console.log("changing from sortedDesc to sortedAsc")
+            console.log(balance_list_desc)
+            console.log(testList_2)
+            changeOrder(balance_list_desc,testList_2)
+        } else {
+            console.log("changing from sortedAsc to sortedDesc")
+            console.log(balance_list_asc)
+            console.log(testList_1)
+            changeOrder(balance_list_asc,testList_1)
+        }
+    }, 1000);
+}
 transaction_date = document.querySelector("#ccDetailsTransactionAccountAccess > thead > tr > th.ccTransDate.ptTableFirstColumnHeader > a")
 transaction_date.setAttribute("id", "sortedAnchor")
 
@@ -39,116 +61,54 @@ posted_table.getElementsByTagName("tr")[0].appendChild(new_col)
 
 transaction_table = document.querySelector("#ccDetailsTransactionAccountAccess > tbody")
 // console.log(transaction_date.children)
-// transaction_list_asc = transaction_table.children
-// transaction_list_dsc = transaction_list_asc.reverse()
 
 var balance_elem = document.createElement("td")
 // balance_elem.innerText = "294"
 
 var diff_amt = 0;
 var next_diff_amt = 0;
-var balance_list_asc = [];
-var balance_list_desc = []
+
 for(var i = transaction_table.children.length-1; i > 0; i--){
     var row = transaction_table.children[i]
     
-    var debit_amt = document.createElement("td")
-    var credit_amt = document.createElement("td")
-    var row_balance = balance_elem.cloneNode(true)
+    debit_amt = document.createElement("td")
+    credit_amt = document.createElement("td")
+    row_balance = balance_elem.cloneNode(true)
     // console.log(row)
     var amt = row.getElementsByClassName("ccTransAmount")[0].innerText
     // console.log("amt is " + amt)
     
     // var diff_amt = 4.5
-    
+    transaction_row = {debit:"",credit:"",balance:""}
     if (amt.includes("CR")) {
         debit_amt.innerText = ""
         credit_amt.innerText = amt.substring(0,amt.length-3)
+        transaction_row.credit = credit_amt.innerText
         next_diff_amt = parseFloat(credit_amt.innerText.substring(1,))
     } else {
         debit_amt.innerText = amt
+        transaction_row.debit = amt
         credit_amt.innerText = ""
         next_diff_amt = parseFloat(amt.substring(1,)) * (-1)
     }
-    // console.log("going to do " + diff_amt)
-    // console.log("current balance is " + current_balance)
-    // console.log("add is " + diff_amt)
-    // console.log("total: " + current_balance + diff_amt)
+  
     current_balance = current_balance + diff_amt
     row_balance.innerText = current_balance
-    // row.appendChild(debit_amt)
-    // row.appendChild(credit_amt)
-    // row.appendChild(row_balance)
+    
     diff_amt = next_diff_amt
 
-    var thisRow = {debit: debit_amt, credit: credit_amt, balance: row_balance}
+    thisRow = {debit: debit_amt, credit: credit_amt, balance: row_balance}
     balance_list_desc.push(thisRow)
+    transaction_list.push(transaction_row)
+    testList_1.push(amt)
+    testList_2.push(amt)
 }
-// balance_list.pop()
-var balance_list_asc = balance_list_desc.slice().reverse()
+balance_list_asc = balance_list_desc.slice().reverse()
+
+
 // balance_list_desc.unshift({debit: 0.0, credit: 0.0, balance: 0.0})
-changeOrder(balance_list_asc)
-// console.log(balance_list_asc)
-// console.log(balance_list_desc)
+// changeOrder(balance_list_asc,testList_1)
+myFunction()
+console.log(balance_list_asc)
 
-// Options for the observer (which mutations to observe)
-var config = { attributes: true};
-
-// Callback function to execute when mutations are observed
-var callback = function(mutationsList, observer) {
-    for(var mutation of mutationsList) {
-        if (mutation.type == 'childList') {
-            console.log('A child node has been added or removed.');
-        }
-        else if (mutation.type == 'attributes') {
-            console.log('The ' + mutation.attributeName + ' attribute was modified.');
-            alert("mutation has finished")
-            
-            if (transaction_date.hasAttribute("sortedDesc")){
-                // alert("changing from sortedAsc to sortedDesc")
-                changeOrder(balance_list_desc)
-            } else {
-                // alert("changing from sortedDesc to sortedAsc")
-                changeOrder(balance_list_asc)
-            }
-        }
-    }
-};
-
-// Create an observer instance linked to the callback function
-var observer = new MutationObserver(callback);
-
-// Start observing the target node for configured mutations
-observer.observe(transaction_date, config);
-// function myFunction() {
-//     if (transaction_date.hasAttribute("sortedDesc")){
-//         alert("changing from sortedDesc to sortedAsc")
-//         changeOrder(balance_list_desc)
-//     } else {
-//         alert("changing from sortedAsc to sortedDesc")
-//         changeOrder(balance_list)
-//     }
-// }
-// sortedAnchor.addEventListener('click', myFunction, false);
-
-// balance_list = balance_list.reverse()
-// console.log(transaction_table.children.length+"------------------------- " + balance_list.length)
-// console.log(balance_list)
-// var index=0
-// for(var j = 1; j < balance_list.length; j++){
-//     var row = transaction_table.children[j]
-//     console.log(j)
-//     // var debit_amt = document.createElement("td")
-//     // var credit_amt = document.createElement("td")
-//     // var row_balance = balance_elem.cloneNode(true)
-//     // console.log(balance_list[j])
-//     debit_amt = balance_list[index].debit
-//     credit_amt = balance_list[index].credit
-//     row_balance = balance_list[index].balance
-    
-//     row.appendChild(debit_amt)
-//     row.appendChild(credit_amt)
-//     row.appendChild(row_balance)
-//     index = index + 1
-// }
-// console.log("end")
+sortedAnchor.addEventListener('click', myFunction);
